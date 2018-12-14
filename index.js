19e325743f68225e9de1e2dc91b0487695119643
Ishmael Carter <icarter@bvtravel.com>
2018-12-14 15:30:04 -0700

    testing users automation

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ T.get('friends/ids', {screen_name: 'NBABot98011077'},function (err, data, respon
 
     T.get('statuses/user_timeline', {user_id: element , count: 1}, function(err, data, response){
       console.log(data)
+      T.post('statuses/retweet/:id', {id: element.id}, function(err, data, response){
+        console.log(data)
+      })
     })
 
   })
