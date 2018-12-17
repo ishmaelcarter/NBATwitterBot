66e661d912a33dc300b8fdba0fb19879cb453188
Ishmael Carter <icarter@bvtravel.com>
2018-12-17 13:59:30 -0700

    Auto tweet verbage

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ T.get('friends/ids', {screen_name: 'NBAatNight'},function (err, data, response){
             T.post('statuses/retweet/:id', {id: retweet}, function(err, data, response){
               console.log(data);
             })
-            T.post('statuses/update', { status: "@" + screen_name + " " + "is on Twitter late!" }, function(err, data, response) {
+            T.post('statuses/update', { status: "@" + screen_name + " " + "was on Twitter late again recently" }, function(err, data, response) {
               console.log(data)
             })
           }
