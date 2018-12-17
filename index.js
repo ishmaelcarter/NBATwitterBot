e706b043915794b831ee792954af87028deff889
Ishmael Carter <icarter@bvtravel.com>
2018-12-17 13:02:24 -0700

    Whew. Automated retweeting all friendship most recent tweets

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 var Twit = require('twit')
 var T = new Twit({
-    consumer_key:         process.env.CONSUMER_KEY,
-    consumer_secret:      process.env.CONSUMER_SECRET,
-    access_token:         process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+    consumer_key:         'f7kgByrtb2nqtWUSgRAaU7Kur',
+    consumer_secret:      'tu9QzGb2C1xwloWIDFqA1Dh3toFO4BFc0qOOO8q6tMfAdY7sdP',
+    access_token:         '1067520612820602882-2sCHP6qzJcKjhP1ANSAqw9FgwDytyP',
+    access_token_secret:  'pOhTfQ75M16yh17bZqw1gglvEb8MArK67Ke8QM0OlCErJ',
 })
 
 T.get('friends/ids', {screen_name: 'NBABot98011077'},function (err, data, response){
   console.log(data)
   var users = data
-  console.log(users);
   users.ids.forEach(function(element) {
-    T.post('friendships/create', {user_id: element}, function (err, data, response){
-      console.log(data)
-    })
-
+    console.log(element);
     T.get('statuses/user_timeline', {user_id: element , count: 1}, function(err, data, response){
-      console.log(data)
-      tweet = data.id
-      T.post('statuses/retweet/:id', {id: tweet}, function(err, data, response){
-        console.log(data)
+      console.log(data[0])
+      retweet = data[0].id_str
+      T.post('statuses/retweet/:id', {id: retweet}, function(err, data, response){
+        console.log(data);
       })
     })
 
