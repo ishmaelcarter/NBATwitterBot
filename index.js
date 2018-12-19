f30f21ae2338d63ad3ee3f3d46929c1dbd991f95
Ishmael Carter <icarter@bvtravel.com>
2018-12-19 10:00:49 -0700

    Async tweet and retweet needs refining

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ T.get('friends/ids', {screen_name: 'NBAatNight'},function (err, data, response){
       screen_name = data[0].user.screen_name
       retweet = data[0].id_str
       retweeted = data[0].retweeted
-      console.log(screen_name);
+      console.log(screen_name)
       if (hour > "22" || hour < "02") {
         if (retweeted) {
 
           } else {
             T.post('statuses/retweet/:id', {id: retweet}, function(err, data, response){
-              console.log(data);
-              T.post('statuses/update', { status: "@" + screen_name + " " + "was on Twitter late again last night" }, function(err, data, response) {
-                console.log(data)
-              })
+              console.log(data)
+            })
+            T.post('statuses/update', { status: "@" + screen_name + " " + "was on Twitter late again last night" }, function(err, data, response) {
+              console.log(data)
             })
           }
         }
