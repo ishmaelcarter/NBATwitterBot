1d30b1a12fce245f16907ce0b689b764a78e8cb4
Ishmael Carter <icarter@bvtravel.com>
2019-02-20 10:35:59 -0700

    Counting number of tweets by day along with tweet times

--- index.js
+++ index.js
@@ -11,28 +11,30 @@ T.get('friends/ids', {screen_name: 'NBAatNight'},function (err, data, response){
   var users = data
   users.ids.forEach(function(element) {
     console.log(element);
-    T.get('statuses/user_timeline', {user_id: element , count: 1}, function(err, data, response){
-      time = data[0].created_at
-      hour = time.substr(11, 2)
+    T.get('statuses/user_timeline', {user_id: element , count: 3}, function(err, data, response){
+      created_1 = data[0].created_at
+      created_2 = data[1].created_at
+      created_3 = data[2].created_at
+      day_1 = created_1.substr(0,10)
+      day_2 = created_2.substr(0,10)
+      day_3 = created_3.substr(0,10)
+      hour = created.substr(11, 2)
       hour = parseInt(hour)
       user = data[0].user.name
       retweet = data[0].id_str
+      retweets = data[0].retweet_count
       retweeted = data[0].retweeted
       console.log(user)
-      if (hour > 22 || hour < 03) {
         if (retweeted) {
           console.log("No new tweets to see")
-          } else {
-            T.post('statuses/retweet/:id', {id: retweet}, function(err, data, response){
-              console.log(data)
-            })
-            messages = ["was on Twitter late again last night", "may not be well rested today", "needs to take a break from the late tweets", "might be tired today", "get to bed a bit earlier"]
+          }
+        else if (day_1 == day_2) {
+            messages = ["was on Twitter a lot yesterday", "was active on Twitter yesterday", "may not be well rested today", "needs to take a break from tweeting", "might be tired today", "get off of twitter a bit earlier", "has tweeted a lot this week"]
             message = messages[Math.floor(Math.random()*messages.length)];
-            T.post('statuses/update', { status: user + " " + message }, function(err, data, response) {
+            T.post('statuses/update', { status: user + " " + message , function(err, data, response) {
               console.log(user)
             })
           }
-        }
     })
 
   })
