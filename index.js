d00bf3e8bb009f2ef014f98eb383c3b4d93d8212
Ishmael Carter <icarter@bvtravel.com>
2019-02-21 11:44:22 -0700

    Updating bot tweet messages

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ T.get('friends/ids', {screen_name: 'NBAatNight'},function (err, data, response){
             T.post('statuses/retweet/:id', {id: retweet}, function(err, data, response){
                 console.log(data)
             })
-            messages = ["was on Twitter a lot yesterday", "was active on Twitter yesterday", "may not be well rested today", "needs to take a break from tweeting", "might be tired today", "get off of twitter a bit earlier", "has tweeted a lot this week"]
+            messages = ["was on Twitter a lot yesterday", "was active on Twitter yesterday", "may not be well rested today", "needs to take a break from tweeting", "might be tired today", "tweeted multiple times yesterday", "has tweeted a lot this week","has been very active"]
             message = messages[Math.floor(Math.random()*messages.length)];
             T.post('statuses/update', { status: user + " " + message , function(err, data, response) {
               console.log(user)
