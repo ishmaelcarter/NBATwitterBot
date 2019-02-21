b39178dde5d3f7752f1d538c688218ab128ec23c
Ishmael Carter <icarter@bvtravel.com>
2019-02-21 11:18:26 -0700

    Logic for empty endpoints

--- index.js
+++ index.js
@@ -12,32 +12,34 @@ T.get('friends/ids', {screen_name: 'NBAatNight'},function (err, data, response){
   users.ids.forEach(function(element) {
     console.log(element);
     T.get('statuses/user_timeline', {user_id: element , count: 3}, function(err, data, response){
-      created_1 = data[0].created_at
-      created_2 = data[1].created_at
-      created_3 = data[2].created_at
-      day_1 = created_1.substr(0,10)
-      day_2 = created_2.substr(0,10)
-      day_3 = created_3.substr(0,10)
-      hour = created_1.substr(11, 2)
-      hour = parseInt(hour)
-      user = data[0].user.name
-      retweet = data[0].id_str
-      retweets = data[0].retweet_count
-      retweeted = data[0].retweeted
-      console.log(user)
-        if (retweeted) {
-          console.log("No new tweets to see")
-          }
-        else if (day_1 == day_2 || day_2 == day_3) {
-          T.post('statuses/retweet/:id', {id: retweet}, function(err, data, response){
-              console.log(data)
+      if (data) {
+        created_1 = data[0].created_at
+        created_2 = data[1].created_at
+        created_3 = data[2].created_at
+        day_1 = created_1.substr(0,10)
+        day_2 = created_2.substr(0,10)
+        day_3 = created_3.substr(0,10)
+        hour = created_1.substr(11, 2)
+        hour = parseInt(hour)
+        user = data[0].user.name
+        retweet = data[0].id_str
+        retweets = data[0].retweet_count
+        retweeted = data[0].retweeted
+        console.log(user)
+          if (retweeted) {
+            console.log("No new tweets to see")
+            }
+          else if (day_1 == day_2 || day_2 == day_3) {
+            T.post('statuses/retweet/:id', {id: retweet}, function(err, data, response){
+                console.log(data)
             })
-          messages = ["was on Twitter a lot yesterday", "was active on Twitter yesterday", "may not be well rested today", "needs to take a break from tweeting", "might be tired today", "get off of twitter a bit earlier", "has tweeted a lot this week"]
-          message = messages[Math.floor(Math.random()*messages.length)];
-          T.post('statuses/update', { status: user + " " + message , function(err, data, response) {
-            console.log(user)
-          }
-       })
+            messages = ["was on Twitter a lot yesterday", "was active on Twitter yesterday", "may not be well rested today", "needs to take a break from tweeting", "might be tired today", "get off of twitter a bit earlier", "has tweeted a lot this week"]
+            message = messages[Math.floor(Math.random()*messages.length)];
+            T.post('statuses/update', { status: user + " " + message , function(err, data, response) {
+              console.log(user)
+            }
+         })
+        }
       }
     })
   })
