6959b4ceb60c0c1d9890c0d317d6071b5ab0ab55
Ishmael Carter <icarter@bvtravel.com>
2019-07-10 08:26:57 -0700

    Refactor for strict mode

--- sql_insert.js
+++ sql_insert.js
@@ -1,17 +1,20 @@
+'use strict';
 var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
-});
+
 
 var Twit = require('twit')
+
 var T = new Twit({
-    consumer_key:         process.env.CONSUMER_KEY,
-    consumer_secret:      process.env.CONSUMER_SECRET,
-    access_token:         process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+  consumer_key:         'f7kgByrtb2nqtWUSgRAaU7Kur',
+  consumer_secret:      'tu9QzGb2C1xwloWIDFqA1Dh3toFO4BFc0qOOO8q6tMfAdY7sdP',
+  access_token:         '1067520612820602882-2sCHP6qzJcKjhP1ANSAqw9FgwDytyP',
+  access_token_secret:  'pOhTfQ75M16yh17bZqw1gglvEb8MArK67Ke8QM0OlCErJ',
+})
+var con = mysql.createConnection({
+host: 'us-cdbr-iron-east-03.cleardb.net',
+user: 'b0943139dae1e6',
+password: '0f1af6bb',
+database: 'heroku_121b5e7e1db5572'
 });
 
 
@@ -19,15 +22,16 @@ db_insert(T, con);
 function db_insert(T, con) {
   T.get('trends/place', {id: 23424977}, function (err, data, response){
     var top_trends = []
-    for (i=0;i<data[0].trends.length;i++) {
-      var volume = data[0].trends[i].tweet_volume;
+    for (var i=0;i<data[0].trends.length;i++) {
+      var j = i;
+      var volume = data[0].trends[j].tweet_volume;
       if (volume > 1000) {
-        top_trends.push(data[0].trends[i].name);
+        top_trends.push(data[0].trends[j].name);
       }
     }
     console.log(top_trends);
     top_trends.forEach(function(element) {
-      T.get('search/tweets', {q: element, count: 1000}, function (err,data,response){
+      T.get('search/tweets', {q: element, count: 10}, function (err,data,response){
        if (data) {
          var tweets;
          tweets = data.statuses ? data.statuses : ["empty"];
@@ -36,6 +40,7 @@ function db_insert(T, con) {
          var media;
          tweets.forEach(tweet => {
            if (tweet) {
+             console.log(tweet.id);
             const lang = tweet.lang;
             var text = tweet.text;
             text = text.substring(text.indexOf(":")+1);
@@ -45,8 +50,8 @@ function db_insert(T, con) {
               var media_url = tweet.entities.media[0].media_url_https;
               var retweets = tweet.retweeted_status.retweet_count;
               var favs = tweet.retweeted_status.favorite_count;
-              console.log([retweets, favs])
-              if (retweets > 500 || favs > 1500) {
+              //console.log([text, lang, media_url, retweets, favs])
+              if (retweets > 200 || favs > 500) {
                 if (lang == "en") {
                   if (media_url.includes("video_thumb") !== true) {
                     var query = "INSERT IGNORE INTO tweets (text, time, retweets, favs, media, url) VALUES (?, ?, ?, ?, ?, ?);";
@@ -55,9 +60,9 @@ function db_insert(T, con) {
                       if (err) {
                         console.log(err);
                       } else {
-                        console.log("Insert Complete: " + + values);
+                        console.log("Insert Complete: " + values);
                       }
-                    }); 
+                    });
                   }
                 }
               }
